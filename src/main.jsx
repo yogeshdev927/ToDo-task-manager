5a92cad9b4dc1d8574f78133d22ef5cc5cf2e0ec
Yogesh Kumar <yogesh91927@gmail.com>
2026-08-31 15:53:51 +0530

    Fix login import path

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {Login} from './components/login/login.jsx'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
